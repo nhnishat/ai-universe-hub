5b1881f0122658dc7f281db55e6fd75c56467657
nhnishat <nhnishat1998@gmail.com>
2023-03-03 02:35:12 +0600

    3rd commit

--- js/script.js
+++ js/script.js
@@ -13,14 +13,28 @@ const displayTools = (data) => {
 		toolsDiv.classList.add('col');
 		toolsDiv.innerHTML = `
         <div class="card h-100">
-            <img src="..." class="card-img-top" alt="..." />
+            <img src="${tool.image}" class="card-img-top " alt="..." />
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title fs-2">Features</h5>
                 <p class="card-text">
-                    This is a longer card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
+                    1. ${tool.features[0]}
                 </p>
+                <p class="card-text">
+                    1. ${tool.features[2]}
+                </p>
+                <p class="card-text">
+                    1. ${tool.features[3]}
+                </p>
+            </div>
+            <hr>
+            <div class="d-flex  justify-content-between align-items-center mx-4"  >
+                <div>
+                    <h5 class="card-title fs-2 my-4">${tool.name}</h5>
+                    <p class="my-4"> <i class="fa-regular fa-calendar-days"></i> ${tool.published_in}</p>
+                </div>
+                <div>
+                    <i class="fa-solid fa-arrow-right fs-2 text-warning"></i>
+                </div>
             </div>
         </div>
         `;
