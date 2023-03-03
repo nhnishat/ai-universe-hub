52e158164a74527179b63bd3d396349a8b9f843a
nhnishat <nhnishat1998@gmail.com>
2023-03-03 17:58:32 +0600

    emni

--- js/script.js
+++ js/script.js
@@ -8,39 +8,48 @@ const displayTools = (tools) => {
 	console.log(tools);
 	const toolsContainer = document.getElementById('tools-container');
 	toolsContainer.innerHTML = '';
-	tools.forEach((tool) => {
+	tools.forEach((singleTool) => {
 		// console.log(tool);
 		const toolsDiv = document.createElement('div');
 		toolsDiv.classList.add('col');
 		toolsDiv.innerHTML = `
         <div class="card h-100">
-            <img src="${tool.image}" class="card-img-fluid h-100" alt="..." />
+            <img src="${singleTool.image}" class="card-img-fluid h-100" alt="..." />
             <div class="card-body">
                 <h5 class="card-title fs-2">Features</h5>
                 <p class="card-text">
-                    1. ${tool.features[0]}
+                    1. ${singleTool.features[0]}
                 </p>
                 <p class="card-text">
-                    2. ${tool.features[1]}
+                    2. ${singleTool.features[1]}
                 </p>
                 <p class="card-text">
-                    3. ${tool.features[2]}
+                    3. ${singleTool.features[2]}
                 </p>
             </div>
             <hr>
             <div class="d-flex  justify-content-between align-items-center mx-4"  >
                 <div>
-                    <h5 class="card-title fs-2 my-4">${tool.name}</h5>
-                    <p class="my-4"> <i class="fa-regular fa-calendar-days"></i> ${tool.published_in}</p>
+                    <h5 class="card-title fs-2 my-4">${singleTool.name}</h5>
+                    <p class="my-4"> <i class="fa-regular fa-calendar-days"></i> ${singleTool.published_in}</p>
                 </div>
                 <div>
-                    <i class="fa-solid fa-arrow-right fs-2 text-warning"></i>
+                    <i onclick="loadToolsDetails('${singleTool.id}')" class="fa-solid fa-arrow-right fs-2 text-warning" data-bs-target="#toolsDetailsModal"data-bs-toggle="modal" data-bs-target="#toolsDetailsModal"></i>
+					
                 </div>
             </div>
         </div>
         `;
 		toolsContainer.appendChild(toolsDiv);
 	});
+};
+// Modal
+const loadToolsDetails = async (id) => {
+	// console.log(id);
+	const url = ` https://openapi.programming-hero.com/api/ai/tool/${id}`;
+	const res = await fetch(url);
+	const data = await res.json();
+	displayToolsDetails(data.data);
 };
 // Slice section
 const aiUniverse = async () => {
@@ -61,4 +70,5 @@ document.getElementById('btn-show-all').addEventListener('click', function () {
 	const btnShow = document.getElementById('show-all');
 	btnShow.classList.add('d-none');
 });
+
 aiUniverse();
