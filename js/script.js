e97d9529ccdb74b6bd3be6b0b6a199cc2057a5ba
nhnishat <nhnishat1998@gmail.com>
2023-03-03 23:40:42 +0600

    emni

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ const displayTools = (tools) => {
         <div class="card h-100">
             <img src="${singleTool.image}" class="card-img-fluid h-100" alt="..." />
             <div class="card-body">
-                <h5 class="card-title fs-2">Features</h5>
+                <h5 class="card-title fs-2 fw-semibold">Features</h5>
                 <p class="card-text">
                     1. ${singleTool.features[0]}
                 </p>
@@ -30,7 +30,7 @@ const displayTools = (tools) => {
             <hr>
             <div class="d-flex  justify-content-between align-items-center mx-4"  >
                 <div>
-                    <h5 class="card-title fs-2 my-4">${singleTool.name}</h5>
+                    <h5 class="card-title fs-2 my-4 fw-semibold">${singleTool.name}</h5>
                     <p class="my-4"> <i class="fa-regular fa-calendar-days"></i> ${singleTool.published_in}</p>
                 </div>
                 <div>
@@ -45,7 +45,6 @@ const displayTools = (tools) => {
 };
 // Modal
 const loadToolsDetails = async (id) => {
-	// console.log(id);
 	const url = ` https://openapi.programming-hero.com/api/ai/tool/${id}`;
 	const res = await fetch(url);
 	const data = await res.json();
@@ -53,9 +52,22 @@ const loadToolsDetails = async (id) => {
 };
 const displayToolsDetails = (tool) => {
 	console.log(tool);
-	const image = document.getElementById('modal-img');
-	image.innerHTML = `
+	const accuracyShow = document.getElementById('accuracy-show');
+	accuracyShow.innerHTML = `
+	${tool.accuracy.score * 100 > 0 ? tool.accuracy.score * 100 : 'No'}% accuracy
+	`;
+	const imageAdded = document.getElementById('modal-img');
+	imageAdded.innerHTML = `
 	<img src="${tool.image_link[0]}" class="img-fluid h-100" alt="">
+	`;
+
+	const modalTextInput = document.getElementById('modal-text-input');
+	modalTextInput.innerHTML = `
+	</h3>${tool.input_output_examples[1].input} </h3>
+	`;
+	const modalTextOutput = document.getElementById('modal-text-output');
+	modalTextOutput.innerHTML = `
+	<p>${tool.input_output_examples[1].output}</p>
 	`;
 };
 // Slice section
@@ -78,4 +90,4 @@ document.getElementById('btn-show-all').addEventListener('click', function () {
 	btnShow.classList.add('d-none');
 });
 
-aiUniverse();
+// aiUniverse();
